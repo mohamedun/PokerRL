3a8a7611e3c0fb37f4668f069ac365fee844a831
Mo <Mo>
2021-01-18 18:35:35 -0500

    typo

--- PokerRL/eval/br/LocalBRMaster.py
+++ PokerRL/eval/br/LocalBRMaster.py
@@ -39,7 +39,6 @@ class LocalBRMaster(EvaluatorMasterBase):
                   "are non-terminal.")
 
     def evaluate(self, iter_nr):
-        print('in evaluate')
         for mode in self._t_prof.eval_modes_of_algo:
 
             if self._is_multi_stack:
